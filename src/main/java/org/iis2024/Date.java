29a8606012f06eba980b378f9a6ffebc45ea84b6
ignacio_almonte_santos <lilsaint080@gmail.com>
2025-03-10 10:18:49 +0100

    cambio public date de propietario

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -60,11 +60,13 @@ public class Date {
    * @param year The year (should ideally be within the valid range of 1900 to 2050 for validation
    *     to pass).
    */
-  public Date(int day, int month, int year) {
-    this.day = day;
-    this.month = month;
-    this.year = year;
+// Constructor modified by the owner
+  public Date(int dia, int mes, int año) {
+    this.day = dia;
+    this.month = mes;
+    this.year = año;
   }
+
 
   /**
    * Validates the date according to the predefined rules: the year must be within 1900 to 2050, the
